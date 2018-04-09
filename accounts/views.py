16820bef7c6fceaf3f332ac4b33cb370648eb9e1
radeshshiwlochan <radesh0430@gmail.com>
2018-04-08 22:16:47 -0400

    adds code for implementing authentication of users

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 # Create your views here.
 def sign_up( request ):
-	return render( request, 'accounts/signup.html' )
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get( username=request.POST['username'] )
+                return render( request, 'accounts/signup.html', { 'error': 'Account Taken!!'} )
+            except User.DoesNotExist:
+                user = User.objects.create_user( request.POST['username'], password=request.POST['password1'] ) 
+                auth.login( request, user )
+                return redirect( 'home' )   
+        else:
+        	return render( request, 'accounts/signup.html', { 'error': 'Password do not match!!'} )
+    else:
+        # request method is a GET, so render the signup view
+	    return render( request, 'accounts/signup.html' )
 
 def login( request ):
 	return render( request, 'accounts/login.html' )
