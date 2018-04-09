734ac186c0ae2fed859fffc843ed5f6c50822e64
radeshshiwlochan <radesh0430@gmail.com>
2018-04-08 20:58:27 -0400

    adds signup, login, and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def sign_up( request ):
+	return render( request, 'accounts/signup.html' )
+
+def login( request ):
+	return render( request, 'accounts/login.html' )
+
+def logout( request ):
+    return render( request, 'accounts/signout.html' )	
+
