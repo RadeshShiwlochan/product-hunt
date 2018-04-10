942590702c0ae20f45443ba717c6e7bac1fb94d6
radeshshiwlochan <radesh0430@gmail.com>
2018-04-09 22:45:17 -0400

    adds a model for product and sets up Postgres db

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Products( models.Model ):
+    title = models.CharField( max_length=200 )
+    url = models.CharField( max_length=200 )
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField()
+    image = models.ImageField( upload_to='images/' )
+    icon = models.ImageField( upload_to='images/' )
+    body = models.CharField( max_length=1000 )
