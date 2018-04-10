dcc2ea46ba4edf55c73ced73a786aab85e99a62d
radeshshiwlochan <radesh0430@gmail.com>
2018-04-09 21:46:24 -0400

    adds implementation for user login

--- accounts/views.py
+++ accounts/views.py
@@ -21,7 +21,15 @@ def sign_up( request ):
 	    return render( request, 'accounts/signup.html' )
 
 def login( request ):
-	return render( request, 'accounts/login.html' )
+    if request.method == 'POST':
+        user = auth.authenticate( username = request.POST['username'], password = request.POST['password1'] )
+        if user is not None:
+            auth.login( request, user )
+            return redirect( 'home' )
+        else:
+            return render( request, 'accounts/login.html', { 'error': 'Username or password is incorrect!!'} )   	
+    else:
+        return render( request, 'accounts/login.html' )
 
 def logout( request ):
     return render( request, 'accounts/signout.html' )	
