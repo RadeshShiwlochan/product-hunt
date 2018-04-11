99b851dc3c760f69009c54b3eaf3e3f67859c7cd
radeshshiwlochan <radesh0430@gmail.com>
2018-04-11 00:10:37 -0400

    adds implementation for creating a product

--- products/models.py
+++ products/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Products( models.Model ):
-    title = models.CharField( max_length=200 )
-    url = models.CharField( max_length=200 )
+    title = models.CharField( max_length=255 )
+    url = models.TextField()
     pub_date = models.DateTimeField()
-    votes_total = models.IntegerField()
+    votes_total = models.IntegerField(default=1)
     image = models.ImageField( upload_to='images/' )
     icon = models.ImageField( upload_to='images/' )
-    body = models.CharField( max_length=1000 )
-    hunter = models.ForeignKey(User,on_delete=models.CASCADE )
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE )
 
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-def summary( self ):
-    return self.body[:100]
+    def summary( self ):
+        return self.body[:100]
 
-def pub_date_pretty( self ):
-	return self.pub_date.strftime('%b %e %Y')
+    def pub_date_pretty( self ):
+	    return self.pub_date.strftime('%b %e %Y')
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Products
+from django.utils import timezone
 
 # Create your views here.
 
@@ -9,5 +11,29 @@ def home( request ):
 @login_required
 def create( request ):
 	#only users that are logged in can visit this page
+    if request.method == 'POST':
+        title = request.POST['title']
+        body  = request.POST['body']
+        url   = request.POST['url']
+        image = request.FILES['image']
+        icon  = request.FILES['icon']
+        if title and body and url and image and icon:
+            product = Products()
+            product.title = title
+            product.body = body
+            if url.startswith( 'http://' ) or url.startswith( 'https://' ):
+            	product.url = url
+            else:
+                product.url = 'http://' + url
+            product.image = image     
+            product.icon = icon
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save() 
+            return redirect('home')	
+        else:
+            render( request, 'products/createProducts.html', { 'error': 'All fields are required!!' } )        	
+    else:
+        render( request, 'products/createProducts.html' )    	
     return render( request, 'products/createProducts.html' )
 
